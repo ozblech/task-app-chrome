1556347a07959555a9fbb3ae1ea2a58112887269
ozblech <ozblech87@gmail.com>
2020-04-18 14:15:16 +0300

    Updates

---
+++ precache-manifest.ec72b43880fe1a65a5d590c6abdc2d81.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b7511672ffd57e87e10d264a1657a",
+    "url": "/task-app-chrome/index.html"
+  },
+  {
+    "revision": "40a2844ccb719b9ae069",
+    "url": "/task-app-chrome/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c8914d9fb69be41a3d25",
+    "url": "/task-app-chrome/static/css/main.96b624e3.chunk.css"
+  },
+  {
+    "revision": "40a2844ccb719b9ae069",
+    "url": "/task-app-chrome/static/js/2.f803c549.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-app-chrome/static/js/2.f803c549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8914d9fb69be41a3d25",
+    "url": "/task-app-chrome/static/js/main.15404933.chunk.js"
+  },
+  {
+    "revision": "c8b55cad392c5fbfdbe2",
+    "url": "/task-app-chrome/static/js/runtime-main.d2aff215.js"
+  },
+  {
+    "revision": "006522fe5d33edea0febf12d84007c42",
+    "url": "/task-app-chrome/static/media/Add.006522fe.png"
+  },
+  {
+    "revision": "31cb6acd1ed68abe964c078310881d56",
+    "url": "/task-app-chrome/static/media/anonymous.31cb6acd.png"
+  },
+  {
+    "revision": "6afd237308b6db6e72ba750cbf260743",
+    "url": "/task-app-chrome/static/media/completed.6afd2373.png"
+  },
+  {
+    "revision": "9a793b73e48072c816aadbfb88a9e08f",
+    "url": "/task-app-chrome/static/media/mic.9a793b73.png"
+  }
+]);
